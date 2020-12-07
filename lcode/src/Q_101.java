91e34bacb662f3a7dd826886d46099d145de4210
chenk-ss <1510687833@qq.com>
2020-12-07 23:05:02 +0800

    add problems 94-101

---
+++ lcode/src/Q_101.java
@@ -0,0 +1,36 @@
+import util.TreeNode;
+
+/**
+ * @Author chenk
+ * @create 2020/12/7 23:04
+ */
+public class Q_101 {
+    /**
+     * Symmetric Tree
+     *
+     * Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+     *
+     * For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+     *
+     *     1
+     *    / \
+     *   2   2
+     *  / \ / \
+     * 3  4 4  3
+     *
+     *
+     * But the following [1,2,2,null,3,null,3] is not:
+     *
+     *     1
+     *    / \
+     *   2   2
+     *    \   \
+     *    3    3
+     *
+     *
+     * Follow up: Solve it both recursively and iteratively.
+     */
+    public boolean isSymmetric(TreeNode root) {
+        return false;
+    }
+}
---
+++ lcode/src/Q_96.java
@@ -0,0 +1,32 @@
+/**
+ * @Author chenk
+ * @create 2020/12/7 23:00
+ */
+public class Q_96 {
+    /**
+     * Unique Binary Search Trees
+     *
+     * Given n, how many structurally unique BST's (binary search trees) that store values 1 ... n?
+     *
+     * Example:
+     *
+     * Input: 3
+     * Output: 5
+     * Explanation:
+     * Given n = 3, there are a total of 5 unique BST's:
+     *
+     *    1         3     3      2      1
+     *     \       /     /      / \      \
+     *      3     2     1      1   3      2
+     *     /     /       \                 \
+     *    2     1         2                 3
+     *
+     *
+     * Constraints:
+     *
+     * 1 <= n <= 19
+     */
+    public int numTrees(int n) {
+        return 0;
+    }
+}
